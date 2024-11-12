34058f03ebcf95ed659c5f019ceb1325dcb9de4a
hechenghao <hechenghao@redrock.team>
2024-11-12 18:47:43 +0800

    :egg:unused consul

--- rpc/server.go
+++ rpc/server.go
@@ -5,22 +5,46 @@ import (
 	"github.com/HCH1212/tiktok_e-commence_rpc/gen/kitex_gen/auth/authservice"
 	"github.com/HCH1212/tiktok_e-commence_rpc/gen/kitex_gen/user/userservice"
 	"github.com/HCH1212/tiktok_e-commence_rpc/user"
-	"github.com/cloudwego/kitex/pkg/rpcinfo"
 	"github.com/cloudwego/kitex/server"
+	"net"
 )
 
-func authRpc() server.Server {
-	authServer := authservice.NewServer(new(auth.AuthImpl), server.WithRegistry(common()), server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{
-		ServiceName: "auth",
-	}), server.WithReusePort(true)) // TODO 服务注册监听多个端口会出现问题，所以直接复用端口可以解决
+//func authRpc() server.Server {
+//	authServer := authservice.NewServer(new(auth.AuthImpl), server.WithRegistry(common()), server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{
+//		ServiceName: "auth",
+//	}), server.WithReusePort(true)) // TODO 服务注册监听多个端口会出现问题，所以直接复用端口可以解决
+//
+//	return authServer
+//}
+//
+//func userRpc() server.Server {
+//	userServer := userservice.NewServer(new(user.UserImpl), server.WithRegistry(common()), server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{
+//		ServiceName: "user",
+//	}), server.WithReusePort(true))
+//
+//	return userServer
+//}
 
-	return authServer
+// 暂时不使用consul
+func authRpc() server.Server {
+	addr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:8081")
+	if err != nil {
+		panic(err)
+	}
+	return authservice.NewServer(new(auth.AuthImpl), server.WithServiceAddr(addr))
 }
 
 func userRpc() server.Server {
-	userServer := userservice.NewServer(new(user.UserImpl), server.WithRegistry(common()), server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{
-		ServiceName: "user",
-	}), server.WithReusePort(true))
+	addr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:8082")
+	if err != nil {
+		panic(err)
+	}
+	return userservice.NewServer(new(user.UserImpl), server.WithServiceAddr(addr)) // TODO 用端口复用会出问题
 
-	return userServer
+	//2024/11/12 18:38:36.457105 default_server_handler.go:259: [Error] KITEX: processing request error, remoteService=, remoteAddr=127.0.0.1:51852, error=biz error: 用户已存在
+	//2024/11/12 18:38:37.725347 default_server_handler.go:259: [Error] KITEX: processing request error, remoteService=, remoteAddr=127.0.0.1:45904, error=unknown method Register
+	//2024/11/12 18:38:38.927851 default_server_handler.go:259: [Error] KITEX: processing request error, remoteService=, remoteAddr=127.0.0.1:45912, error=biz error: 用户已存在
+	//2024/11/12 18:38:39.865708 default_server_handler.go:259: [Error] KITEX: processing request error, remoteService=, remoteAddr=127.0.0.1:45926, error=unknown method Register
+	//2024/11/12 18:38:40.827435 default_server_handler.go:259: [Error] KITEX: processing request error, remoteService=, remoteAddr=127.0.0.1:45932, error=unknown method Register
+
 }
